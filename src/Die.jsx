8c5a8719813dee2c535c5dd98a48bf2475ac2569
Johan Söderlund <johan.soderlund96@gmail.com>
2024-03-26 00:45:08 +0100

    last styles

--- src/Die.jsx
+++ src/Die.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Die = (props) => {
   const styles = {
-    backgroundColor: props.isHeld ? "#59E391" : "#ffffff",
+    backgroundColor: props.isHeld
+      ? "rgba(89, 227, 145, 0.8)"
+      : "rgba(255, 255, 255, 0.8)",
   };
   return (
     <div className="die-face" style={styles} onClick={props.holdDice}>
